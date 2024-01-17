1d507fb08acd3815ebf640d44621a565d4997a59
Giovanni Sia <76817363+GiovanniSia@users.noreply.github.com>
2024-01-17 00:49:42 -0300

    update animation css

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 // Selecciona la imagen que quieres animar
-let img = document.getElementById('card1-img');
 let imgs = document.querySelectorAll('img');
+let titulos = document.querySelectorAll('h1');
+let parrafos = document.querySelectorAll('p');
 
 // Crea una instancia de Intersection Observer con una función de devolución de llamada
 let observer = new IntersectionObserver((entries, observer) => {
     entries.forEach(entry => {
-        // Verifica si el elemento es visible en el área de visualización
-        if (entry.isIntersecting) {
-            // Añade las clases de animación
-            entry.target.classList.add('animate__animated');
-            entry.target.classList.add('animate__zoomIn');
-
-            // Deja de observar el elemento una vez que se ha activado la animación
+        if (entry.isIntersecting  && !entry.target.classList.contains("portada")) {
+            entry.target.classList.add('acordeon-abajo-arriba');
             observer.unobserve(entry.target);
         }
     });
-}, { threshold: 0.1 }); // El umbral define qué porcentaje del elemento debe ser visible para considerarlo "intersecting"
+}, { threshold: 0 }); // El umbral define qué porcentaje del elemento debe ser visible para considerarlo "intersecting"
 
-// Empieza a observar la imagen
+let observer2 = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting && !entry.target.classList.contains("portada")) {
+            entry.target.classList.add('acordeon-izq-der');
+            observer.unobserve(entry.target);
+        }
+    });
+}, { threshold: 0 });
+
 imgs.forEach(i => observer.observe(i))
+titulos.forEach(i => observer2.observe(i))
+parrafos.forEach(i => observer2.observe(i))
