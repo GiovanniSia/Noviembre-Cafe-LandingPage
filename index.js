286a2e41461dfbe64704b1b250550f065471173b
leonelSubelza <subelza150@gmail.com>
2024-01-18 14:40:15 -0300

    Se arreglan las animaciones

--- index.js
+++ index.js
@@ -1,27 +1,56 @@
 // Selecciona la imagen que quieres animar
-let imgs = document.querySelectorAll('img');
-let titulos = document.querySelectorAll('h1');
-let parrafos = document.querySelectorAll('p');
+let elementosPortada = document.querySelectorAll('.portada');
+let bloque2 = document.querySelectorAll('.bloque2-icon-animation');
+let bloque3 = document.querySelectorAll('.bloque3-icon-animation')
+let contactoItems = document.querySelectorAll('.card-contacto-elem-animation');
 
-// Crea una instancia de Intersection Observer con una función de devolución de llamada
-let observer = new IntersectionObserver((entries, observer) => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting  && !entry.target.classList.contains("portada")) {
-            entry.target.classList.add('acordeon-abajo-arriba');
-            observer.unobserve(entry.target);
-        }
-    });
-}, { threshold: 0 }); // El umbral define qué porcentaje del elemento debe ser visible para considerarlo "intersecting"
 
-let observer2 = new IntersectionObserver((entries, observer) => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting && !entry.target.classList.contains("portada")) {
-            entry.target.classList.add('acordeon-izq-der');
-            observer.unobserve(entry.target);
-        }
-    });
-}, { threshold: 0 });
 
-imgs.forEach(i => observer.observe(i))
-titulos.forEach(i => observer2.observe(i))
-parrafos.forEach(i => observer2.observe(i))
+function crearIntersectionObserver(clases) {
+    return new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting){
+                clases.forEach(c => entry.target.classList.add(c));
+                observer.unobserve(entry.target);
+            }
+            /*
+            //Arreglar todo esto
+            //Funciona para todo el resto
+            if (entry.isIntersecting && !entry.target.classList.contains("portada")
+            && !entry.target.classList.contains("card-contacto-elem-animation")
+            && !entry.target.classList.contains("bloque2_img-img")) {
+                clases.forEach(c => entry.target.classList.add(c));
+                observer.unobserve(entry.target);
+            }
+            //Solo funciona para Img de comentarios
+            if(entry.isIntersecting && entry.target.classList.contains("bloque2_img-img")){
+                clases.forEach(c => entry.target.classList.add('acordeon-izq-der'));
+                observer.unobserve(entry.target);
+            }
+
+            //Solo funciona para Icon de contactos
+            if(entry.isIntersecting && clases[0]==='animate__animated' && entry.target.classList.contains("card-contacto-elem-animation")){
+                clases.forEach(c => entry.target.classList.add(c));
+                observer.unobserve(entry.target);
+            }
+
+            //Solo funciona para animacion de portada
+            if(entry.isIntersecting && clases[0]==='custom-pulse' && entry.target.classList.contains("portada") 
+            && !entry.target.classList.contains("portada-logo")){
+                clases.forEach(c => entry.target.classList.add(c));
+                observer.unobserve(entry.target);
+            }
+            */
+        });
+    }, { threshold: 0 });
+}
+
+const observerPortadaAnimation = crearIntersectionObserver(['custom-pulse']);
+const observerAcordeonAbArAnimation = crearIntersectionObserver(['acordeon-abajo-arriba']);
+const observerAcordeonIzDerAnimation = crearIntersectionObserver(['acordeon-izq-der']);
+const observerContactoAnimation = crearIntersectionObserver(['animate__animated','animate__heartBeat']);
+
+bloque2.forEach(i => observerAcordeonIzDerAnimation.observe(i));
+bloque3.forEach(i => observerAcordeonAbArAnimation.observe(i));
+contactoItems.forEach(i => observerContactoAnimation.observe(i));
+elementosPortada.forEach(i => observerPortadaAnimation.observe(i));
